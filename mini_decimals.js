ed3f26c9e8f8db7f63adce7911620fa3f1544f5d
xsincerex1990 <xsincerex1990@email.com>
2018-08-02 23:09:06 -0400

    JAVASCRIPT BUG

---
+++ mini_decimals.js
@@ -0,0 +1,8 @@
+//side effect of using binary floating-point numbers
+//Mathematically, we know that statement should be true. Why is it false?
+//the representations for 0.1 and 0.2 in binary floating-point are not exact, so when they are added,
+//the result is not exactly 0.3. It's really close: 0.30000000000000004,
+//but if your comparison fails, "close" is irrelevant.
+0.1 + 0.2 === 0.3; // false
+
+
--- numbers0.js
+++
@@ -1,3 +0,0 @@
-var a = [1, 3, ["a", "b"]]
-
-console.log(a[2][0])
