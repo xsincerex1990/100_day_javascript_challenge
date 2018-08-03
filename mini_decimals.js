478885a9e34876ef728b0bfe618ee73544bc5717
xsincerex1990 <xsincerex1990@email.com>
2018-08-02 23:44:12 -0400

    DECIMALS

--- mini_decimals.js
+++ mini_decimals.js
@@ -3,6 +3,7 @@
 //the representations for 0.1 and 0.2 in binary floating-point are not exact, so when they are added,
 //the result is not exactly 0.3. It's really close: 0.30000000000000004,
 //but if your comparison fails, "close" is irrelevant.
+//WEIRD SHIT?
 0.1 + 0.2 === 0.3; // false
-
-
+0.4 + 0.5 === 0.9; //true
+0.3 + 0.6 === 0.9; //false
