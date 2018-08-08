d966312bb6712292ab0b0fc7d8d803bf479e4024
xsincerex1990 <xsincerex1990@gmail.com>
2018-08-08 11:23:09 -0400

    value-copy & reference

--- value_n_reference.js
+++ value_n_reference.js
@@ -34,6 +34,80 @@ a; // [1,2,3]
 b; // [4,5,6] "b" is NOT a POINTER to an array theres no such capability.
 
 
+/** This example below is a common confusion with function parameters**/
+ function foo(x) {
+	 x.push(4);
+	 x; //[1, 2, 3, 4]
+
+	 x = [4, 5, 6]
+	 x.push(7);
+	 x; //[4, 5, 6, 7]
+}
+
+var a = [1, 2, 3]
+foo(a);// [1, 2, 3, 4] [4, 5, 6, 7]
+
+a; /** [1, 2, 3, 4] (not the same as foo(a) output) see Kyles types & grammar 
+for more info (ch2)**/
+
+/** To accomplish changing a to have [4, 5, 6, 7] you cant create a new array, 
+ * YOU MUST MODIFY the existing array**/
+
+function foo(x) {
+	x.push(4);
+	x; // [1,2,3,4]
+        
+        // x.length & x.push(4,5,6,7) were not creating a new array!!
+	// IT WAS MODIFYING THE EXISTING ARRAY
+
+	x.length = 0; // empty existing array in-place
+	x.push(4, 5, 6, 7);
+	x; // [4,5,6,7]
+}
+
+var soup = [1,2,3];
+
+foo( soup );
+
+soup; // [4,5,6,7]   
+
+/** To effectively pass a value like array by value-copy you need to manually
+ * make a copy of it so that the reference passed doesn't still point 
+ * to the original. For example: **/
+
+
+//Be RIGHT BACK
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
