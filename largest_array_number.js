6cd47d4b3c0704bce02a85f065b52e8ff59f0a5a
xsincerex1990 <xsincerex1990@gmail.com>
2018-08-09 22:32:40 -0400

    Find BigNum in arrays

---
+++ largest_array_number.js
@@ -0,0 +1,24 @@
+//This algorithm will find the largest number in an array
+
+var arr = [
+	[845, 745, 935, 124],
+	[647, 638, 345, 035],
+	[777, 666, 1111, 555],
+	[369, 111, 222, 333]
+];
+
+function ifind(arr) {
+	var results = [];
+	for (var n = 0; n < arr.length; n++) {
+		var bignum = arr[n][0];
+		for (var sn = 1; sn < arr[n].length; sn++) {
+			if (arr[n][sn] > bignum) {
+				bignum = arr[n][sn];
+			}
+		}
+	results[n] = bignum;
+	}
+	return results;
+}
+console.log(ifind(arr))
+
