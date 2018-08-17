317f591e76e805fb6a1795a0c5e72b4c2e11e21e
Github Cesar <xsincerex1990@gmail.com>
2018-08-17 18:40:24 -0400

    react buttons

--- react_pratice/src/components/counter.jsx
+++ react_pratice/src/components/counter.jsx
@@ -4,24 +4,38 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: []
     };
+	constructor() {
+		super();
+		this.handleIncrement = this.handleIncrement.bind(this);
+	}
 
-    renderTags() {
-        if (this.state.tags.length === 0 ) return <p>There are no tags!</p>;
+	handleIncrement = () => {
+		this.setState({ count: this.state.count + 1})
+	}
+
+
+
+
+
+	render() { 
+	return (
         
-        return <ul>{ this.state.tags.map(tag => <li key={tag}> { tag } </li>)}</ul>;
-    }
-
-    render() { 
-
-        return (
-        <div>
-            {this.state.length === 0 && "Please create tags!!"}
-            {this.renderTags()}
+	<div>
+	 <span className={this.badges()}> {this.formatCount()}</span>
+	<button onClick={this.handleIncrement} className="btn btn-secondary btn-sm"> Increment</button>
         </div>
         );
     }
+	badges() {
+		let classes = "badge m-2 badge-";
+		classes += this.state.count === 0 ? "warning" : "primary";
+		return classes;
+	}
+	formatCount () {
+		const {count} = this.state;
+		return count === 0 ? 'zero' : count;
+	}
 };
  
 export default Counter 
--- react_pratice/src/index.js
+++ react_pratice/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter'
-    
+import Counters from './components/Counters'
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+
+ReactDOM.render(<Counters />, document.getElementById('root'));
 registerServiceWorker();
