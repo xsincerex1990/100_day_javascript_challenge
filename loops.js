852d98267f3543a0ef0373294f5e91dfe2dad2d3
Github Cesar <xsincerex1990@gmail.com>
2018-08-19 17:20:43 -0400

    looping through array

--- loops.js
+++ loops.js
@@ -1,10 +1,12 @@
+//adding everything in an array with a loop
+
+
 let numbers = [3, 7, 12, 22, 100];
 var sum = 0;
 
 for (var i = 0; i < numbers.length; ++i) {
-	console.log(i)
-	sum = sum + numbers[i];
+	sum += numbers[i];
 
 }
 
-console.log(sum)
+console.log(sum) //144 
