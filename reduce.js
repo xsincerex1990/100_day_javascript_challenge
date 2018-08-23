c604d19d129d94970e42453d194fcebebe2b994e
Github Cesar <xsincerex1990@gmail.com>
2018-08-23 18:03:36 -0400

    reducing stuff

---
+++ reduce.js
@@ -0,0 +1,26 @@
+var orders = [
+	{ amount: 250 },
+	{ amount: 400 },
+	{ amount: 100 },
+	{ amount: 325 }
+]
+/*The loop below is one of the things loop does behind the scenes...
+ 
+var total = 0
+for (var i = 0; i < orders.length; i++) {
+	total += orders[i].amount
+}
+console.log(total) // 1075 
+*/
+
+
+/*Much easier with reduce..like map & filter, reduce takes a callback function but it wants an object in this the zero.
+You can think of this zero as the  starting point for reduce to start its count */
+
+
+
+var total = orders.reduce(function(sum, order) {
+	return sum + order.amount}, 0)
+console.log(total) // 1075 
+
+// above the reduce function takes two arguments the first is where you will store your new values in this case the sum of the amounts. the second is 
