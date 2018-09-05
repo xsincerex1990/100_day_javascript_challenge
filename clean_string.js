0f8628ce48cd9bd58dda33ad8726f783b9c58ef3
Github Cesar <xsincerex1990@gmail.com>
2018-09-05 15:45:04 -0400

    Removing ints from string

--- clean_string.js
+++ clean_string.js
@@ -1,7 +1,17 @@
+//Remove all numbers from a string by king sanchez
+
 let str = "uchnui5487ywh54guhghuirt854h"
 let arr = str.split('')
-let c_arr = arr.filter(function (item) {
-        return !(parseInt(item) == item); 
-})
-console.log(c_arr)
 
+function np(arr1) {
+    for (let i = 0; arr.length > i; i++) {
+        if (parseInt(arr[i])) {
+            nums.push(arr[i])
+        }  
+    }
+}
+
+
+let nums = []
+np(arr);
+console.log(nums) // [ '5', '4', '8', '7', '5', '4', '8', '5', '4' ]
