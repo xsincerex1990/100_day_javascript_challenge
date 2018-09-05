991a608f1b34efd1dd0903a2b84433fd9aea56e7
Github Cesar <xsincerex1990@gmail.com>
2018-09-05 16:24:48 -0400

    Pull numbers from string

--- clean_string.js
+++ clean_string.js
@@ -1,17 +1,12 @@
-//Remove all numbers from a string by king sanchez
+//Remove all numbers from a string
 
 let str = "uchnui5487ywh54guhghuirt854h"
 let arr = str.split('')
-
-function np(arr1) {
-    for (let i = 0; arr.length > i; i++) {
-        if (parseInt(arr[i])) {
-            nums.push(arr[i])
-        }  
-    }
-}
-
-
 let nums = []
-np(arr);
-console.log(nums) // [ '5', '4', '8', '7', '5', '4', '8', '5', '4' ]
+
+for (let i = 0; arr.length > i; i++) {
+    if (parseInt(arr[i])) {
+        nums.push(arr[i])
+    } 
+}
+console.log(nums)
