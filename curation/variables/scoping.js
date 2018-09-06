820bf43c053c3c685b615c677370a568fdd084e4
Github Cesar <xsincerex1990@gmail.com>
2018-09-05 20:09:35 -0400

    review scoping

--- curation/variables/scoping.js
+++ curation/variables/scoping.js
@@ -1,19 +1,17 @@
 // scoping 
 
 function scopetest()  { 
-  //console.log(1, x)// 
   let x
-  console.log(2, x)
+  console.log(1, x) // undefined-- because x was declared but not defined
   x = 42
-  console.log(3, x) 
+  console.log(2, x) // 2, 42--because JS reads line by line and ir read the x definition
   if (true) {
-  // console.log(4, x)
-    let x
-    console.log(5, x)
+    let x;
+    console.log(3, x) // undefined  
     x = 17
-    console.log(6, x) 
+    console.log(4, x) // 4, 17 
    }
-    console.log(7, x)
+    console.log(5, x) // 5, 42-- because its the last line on the scope of sopetest()
 }
 
 scopetest() 
