d344d9922db4e3757ff3042de106fc42356cfb4d
Github Cesar <xsincerex1990@gmail.com>
2018-09-05 20:27:14 -0400

    notes

---
+++ include.js
@@ -0,0 +1,5 @@
+// Does string have a specific Number or character?? Lets find out.
+
+let str = "uuch87gw8t74yf875y8t7hg8ntgy3n865mfm6igucwdih874g7t8hfn84rcy87f4ytn8d0yt78y64tdgy58yg"
+
+console.log(str.includes("9"));
