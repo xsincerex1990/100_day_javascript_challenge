30af0dbf0a8929e64650a9f24726e238715e6a42
Github Cesar <xsincerex1990@gmail.com>
2018-09-05 21:32:21 -0400

    Computed property

---
+++ computed_property.js
@@ -0,0 +1,15 @@
+let how = "thefuck"
+
+let pObj = {
+    id: "Cheetah",
+    name: "Jose Barbosa",
+    age: 97,
+    [how]: {
+         years: 12,
+         interest: ['food', 'science', 'technology']
+    }
+}
+console.log(pObj.thefuck)
+
+
+
