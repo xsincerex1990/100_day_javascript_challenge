24d0f81b3f65c05552f1cb1ee2d6c93e40415d0c
Github Cesar <xsincerex1990@gmail.com>
2018-09-07 17:49:45 -0400

    getter & setter

---
+++ getter.js
@@ -0,0 +1,13 @@
+let artists = {
+    frames: ["West", "East", "South"],
+    get mix() {
+        if (this.frames.length == 0) {
+            return undefined;
+        }
+        return this.frames[this.frames.length - 1];
+    },  
+};
+Object.defineProperty(artists, "pine", {get: function() {return this.frames[0];}});
+
+console.log(artists.mix); //South
+console.log(artists.pine) // West
