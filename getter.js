b88c3e622b9f02990e92d098b99f0e9197d19e69
Github Cesar <xsincerex1990@gmail.com>
2018-09-07 20:20:46 -0400

    getters & defining properties

--- getter.js
+++ getter.js
@@ -1,13 +1,21 @@
-let artists = {
-    frames: ["West", "East", "South"],
-    get mix() {
+let camp = {
+    frames: ["North", "West", "East", "South"],
+    get direction() {
         if (this.frames.length == 0) {
             return undefined;
         }
         return this.frames[this.frames.length - 1];
     },  
 };
-Object.defineProperty(artists, "pine", {get: function() {return this.frames[0];}});
- 
-console.log(artists.mix); //South
-console.log(artists.pine) // West
+
+
+Object.defineProperty(camp, "pine", {get: function() {return this.frames[0];}});
+Object.defineProperty(camp, "random", {get: function() { 
+    let rand_num = Math.floor(Math.random() * this.frames.length)
+    let rand_dir = this.frames[rand_num]
+    
+    return rand_dir
+}});
+
+
+console.log([camp.random, camp.direction, camp.pine]) 
