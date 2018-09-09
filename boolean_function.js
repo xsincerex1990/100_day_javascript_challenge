a24cec21518e0e45a3db1e037d8c304c98cb6aa0
Github Cesar <xsincerex1990@gmail.com>
2018-09-09 19:38:37 -0400

    Boolean function

---
+++ boolean_function.js
@@ -0,0 +1,10 @@
+//Create object with intial value of false
+
+let false_object = new Boolean();
+
+//Converting a non boolean value to a boolean value
+
+let val = 0
+let a = Boolean(val) //DONT NOT USE BOOLEAN OBJECT TO CONVERT
+console.log(a)
+console.log(false_object);
