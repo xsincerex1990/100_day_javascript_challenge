5afb8c3fb8b5ef50afd58cb1e6166bfdecbab391
Github Cesar <xsincerex1990@gmail.com>
2018-09-14 18:59:13 -0400

    ...Simple REGEX

---
+++ challenge_dir/RegEX.js
@@ -0,0 +1,7 @@
+let string = "Hello, nsfdijoijvfjscgiojfiojsrfodivbmnsfjiaoejgireju09gwujreiwgstaajoasjskdlnvvbiureiue845ygiwus;"
+
+function vacuum(str) {
+    let cx = str.match(/[as]/gi)
+    return cx;
+}
+console.log(vacuum(string));
