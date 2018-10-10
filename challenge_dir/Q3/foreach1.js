ae1023cb241f3dc4b75d57fd2d0bb380fb6017e5
fruitdealer <xsincerex1990@gmail.com>
2018-10-09 22:53:28 -0400

    return numbers in str format

--- challenge_dir/Q3/foreach1.js
+++ challenge_dir/Q3/foreach1.js
@@ -1,7 +1,23 @@
-let names = ["Mark", "Damon", "Joel"]
+// write a function that takes a string and pulls all the numbers from the string and returns the in string format
 
+let x;
+let x1 = '';
+let r = /[0-9]/
 
-names.forEach(function(name) {
-    console.log(name)
-})
+function checker(str) {
+	x = str.split('')
+	x.forEach(function(val) {
+		if (r.test(val)) {
+			x1 += val;
+		}
+	})
+
+	if (x1) {
+		console.log(x1);
+	} else {
+		console.log("No nums in this shit");
+	}
+}
+
+checker("dsj7f");
 
